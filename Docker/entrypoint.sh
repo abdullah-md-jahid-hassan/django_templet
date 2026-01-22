c5055f80dfdcacba53eb70695a6a5efeb8e6b89d
Abdullah Md Jahid Hassan <abdullahmdjahidhassan@gmail.com>
2026-01-22 23:38:54 +0600

    Base tamp

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,23 @@
+
+# entrypoint.sh
+
+#!/bin/sh
+set -e
+
+# Ensure virtualenv is active
+export PATH="/venv/bin:$PATH"
+
+# Only web container should run migrations & collectstatic
+if [ "$RUN_MIGRATIONS" = "1" ]; then
+    echo "Making migration files..."
+    python manage.py makemigrations
+
+    echo "Applying database migrations..."
+    python manage.py migrate --noinput
+
+    # echo "Collecting static files..."
+    # python manage.py collectstatic --noinput || true
+fi
+
+# Execute passed command (gunicorn / celery)
+exec "$@"
